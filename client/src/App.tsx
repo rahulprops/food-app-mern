ee5394c53d7190144e5c69dad618e404592b319d
rahulprops <rahulhack912@gmail.com>
2025-04-09 22:27:19 +0530

    setup

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div className=" text-6xl">
+        kd
+        <Button>click me</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
